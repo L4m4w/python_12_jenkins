9ef13d605c873e25912cffd3b23b5d3d4097f6ed
Evgen Larkin <l4p41@yandex.com>
2025-01-28 21:52:20 +0300

    browser selector

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,28 @@
 import pytest
 from selene import browser
+from selene.support.shared import config
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 from configure.utils import attach
 
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser',
+        help='Браузер, в котором запущены тесты',
+        choices=['firefox', 'chrome'],
+        default='chrome'
+    )
+
+@pytest.fixture(scope='session')
+def browser_name(request):
+    return request.config.getoption('--browser')
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_manager():
+def browser_manager(browser_name):
     options = Options()
     selenoid_capabilities = {
-        "browserName": "chrome",
+        "browserName": browser_name,
         "browserVersion": "100.0",
         "selenoid:options": {"enableVNC": True, "enableVideo": True}
     }
